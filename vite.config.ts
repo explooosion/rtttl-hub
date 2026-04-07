5401e1fe28e94733251fb9474b617cc673ec9f02
Robby Wu <ta7382@gmail.com>
2026-04-08 02:20:21 +0800

    chore: rename project to rtttl-hub, update build config and deps

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/rtttl-player/",
+  base: "/rtttl-hub/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
